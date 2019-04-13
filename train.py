59a0808aacc4dde7d36f65d0dd5ffb9c968a3228
jonathanking <jonathanking@berkeley.edu>
2019-04-13 12:19:26 -0400

    Linefeeds. Workspace.

--- train.py
+++ train.py
@@ -5,7 +5,7 @@ import numpy as np
 from glob import glob
 import os
 import pickle
-from data_loader import get_loader 
+from data_loader import get_loader
 from build_vocab import Vocabulary
 from model import EncoderCNN, DecoderRNN
 from torch.nn.utils.rnn import pack_padded_sequence
@@ -21,7 +21,7 @@ def main(args):
     # Create model directory
     if not os.path.exists(args.model_path):
         os.makedirs(args.model_path)
-    
+
     # Image preprocessing, normalization for the pretrained resnet
     transform = transforms.Compose([
         # transforms.ToPILImage(),
@@ -31,15 +31,15 @@ def main(args):
         transforms.Normalize((0.485, 0.456, 0.406),
                              (0.229, 0.224, 0.225))])\
     # transform = None
-    
+
     # Load vocabulary wrapper
     with open(args.vocab_path, 'rb') as f:
         vocab = pickle.load(f)
-    
+
     # Build data loader
     data_loader = get_loader(args.caption_path, args.image_path,  vocab,
                              transform, args.batch_size,
-                             shuffle=True, num_workers=args.num_workers) 
+                             shuffle=True, num_workers=args.num_workers)
 
     # Build the models
     if not args.gan_embedding:
@@ -54,21 +54,21 @@ def main(args):
             encoder_file = sorted(glob("models/encoder*.ckpt"))[-1]
             print("Loading", encoder_file)
             encoder.load_state_dict(torch.load(encoder_file))
-        decoder_file = sorted(glob("models/decoder*.ckpt"))[-1]
+        decoder_file = sorted(glob("models/decod≤er*.ckpt"))[-1]
         print("Loading", decoder_file)
         decoder.load_state_dict(torch.load(decoder_file))
         starting_i = int(decoder_file.split("-")[2][:-5])
         starting_epoch = int(decoder_file.split("-")[1])
 
 
-    
+
     # Loss and optimizer
     criterion = nn.CrossEntropyLoss()
     params = list(decoder.parameters())
     if not args.gan_embedding:
         params += list(encoder.linear.parameters()) + list(encoder.bn.parameters())
     optimizer = torch.optim.Adam(params, lr=args.learning_rate)
-    
+
     # Train the models
     total_step = len(data_loader)
     for epoch in range(args.num_epochs):
@@ -77,7 +77,7 @@ def main(args):
             images = images.to(device)
             captions = captions.to(device)
             targets = pack_padded_sequence(captions, lengths, batch_first=True)[0]
-            
+
             # Forward, backward and optimize
             if args.gan_embedding:
                 disc, features = gan.discriminator(images)
@@ -94,7 +94,7 @@ def main(args):
             # Print log info
             if i % args.log_step == 0:
                 print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Perplexity: {:5.4f}'
-                      .format(epoch, args.num_epochs, i, total_step, loss.item(), np.exp(loss.item()))) 
+                      .format(epoch, args.num_epochs, i, total_step, loss.item(), np.exp(loss.item())))
 
             if i % args.print_cap_step == 0:
                 with torch.no_grad():
@@ -126,13 +126,13 @@ if __name__ == '__main__':
     parser.add_argument('--save_step', type=int , default=1000, help='step size for saving trained models')
     parser.add_argument('--print_cap_step', type=int, default=50, help='step size for printing captions')
     parser.add_argument('--resume', action="store_true", help='resume model training from most recent checkpoint')
-    
+
     # Model parameters
     parser.add_argument('--embed_size', type=int , default=1024, help='dimension of word embedding vectors')
     parser.add_argument('--hidden_size', type=int , default=512, help='dimension of lstm hidden states')
     parser.add_argument('--num_layers', type=int , default=1, help='number of layers in lstm')
     parser.add_argument('--gan_embedding', action="store_true", help='use a trained GAN to provide image embeddings for the RNN. Use ResNet otherwise.')
-    
+
     parser.add_argument('--num_epochs', type=int, default=5)
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--num_workers', type=int, default=4)
